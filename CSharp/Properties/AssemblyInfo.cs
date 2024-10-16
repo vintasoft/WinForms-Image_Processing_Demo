8817bd92fbe1d1131b6d7b8dd6870d83f24180a7
vintasoft <alex@vintasoft.com>
2024-10-16 15:55:25 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VintaSoft Image Processing Demo")]
 [assembly: AssemblyDescription("Shows how to to process images.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Ltd.")]
+[assembly: AssemblyCompany("VintaSoft Llc.")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
